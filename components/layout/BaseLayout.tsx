0311a6d38b881a57e87587e25118a080ad64cfc8
M4p4 <homejr@protonmail.com>
2022-12-08 15:28:01 +0100

    layout update

--- components/layout/BaseLayout.tsx
+++ components/layout/BaseLayout.tsx
@@ -8,13 +8,13 @@ type Props = {
 
 const BaseLayout: FC<Props> = ({ children }) => {
   return (
-    <div className="bg-orange-50 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-300">
+    <>
       <Navbar />
       <main className="max-w-3xl py-2 mx-auto min-h-screen px-3">
         {children}
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
