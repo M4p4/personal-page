05dd712d3abcfd744a2afff90111eeed06a9c595
M4p4 <homejr@protonmail.com>
2022-12-08 15:26:14 +0100

    image support for mdx (responsive)

--- components/ui/MDXComponents.tsx
+++ components/ui/MDXComponents.tsx
@@ -1,8 +1,18 @@
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 import Link from 'next/link';
 import React, { FC } from 'react';
 
 const MDXComponents = {
+  img: (props: any) => (
+    <Image
+      alt={props.alt}
+      src={props.src}
+      width={1080}
+      height={720}
+      layout={'responsive'}
+      className="rounded-md w-full object-cover"
+    />
+  ),
   a: (props: any) => (
     <Link href={props.href} className="text-orange-600">
       {props.children}
