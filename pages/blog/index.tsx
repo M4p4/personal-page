dc17c8a8ad774a54fe3ba9eb4cc3e1875415ff9d
M4p4 <homejr@protonmail.com>
2022-12-08 06:53:01 +0100

    wip

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,8 +3,8 @@ import Posts from 'components/posts/Posts';
 const BlogPage = () => {
   return (
     <div className="py-2">
-      <h1 className="text-3xl font-semibold dark:text-zinc-100 pt-1 pb-2">
-        Blog Posts
+      <h1 className="text-3xl md:text-4xl font-semibold dark:text-zinc-100 py-3">
+        All Posts
       </h1>
       <Posts />
     </div>
