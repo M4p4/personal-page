784b1a32088697831b5c482e45250a1e03738996
M4p4 <homejr@protonmail.com>
2023-01-05 17:07:42 +0100

    added meta title and desc

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,6 +3,7 @@ import { getAllPosts } from 'lib/MDXLoader';
 import { GetStaticProps } from 'next';
 import { FC } from 'react';
 import { Post } from 'types';
+import NextHeadSeo from 'next-head-seo';
 
 type Props = {
   posts: Post[];
@@ -11,9 +12,24 @@ type Props = {
 const BlogPage: FC<Props> = ({ posts }) => {
   return (
     <>
-      <h1 className="text-3xl md:text-4xl font-bold tracking-tight dark:text-zinc-100 mt-4 mb-4 md:mb-0">
-        All Posts
+      <NextHeadSeo
+        title="Web Development & SEO Blog"
+        description="Welcome to my personal blog where I share my thoughts and experiences on
+        programming and search engine optimization (SEO)."
+      />
+      <h1 className="text-3xl md:text-4xl font-bold tracking-tight dark:text-zinc-100 mt-4 mb-4">
+        Blog
       </h1>
+      <p>
+        Welcome to my personal blog where I share my thoughts and experiences on
+        programming and search engine optimization (SEO). Follow along as I
+        delve into the latest technologies and techniques in these ever-evolving
+        fields and learn how to improve your own skills and website&#39;s
+        visibility.
+      </p>
+      <h2 className="text-2xl font-semibold dark:text-zinc-100 py-4">
+        Latest Posts
+      </h2>
       <Posts posts={posts} />
     </>
   );
