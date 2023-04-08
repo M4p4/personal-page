f4f12ca0f49fc35e188986fcfc9c5b4697167040
M4p4 <homejr@protonmail.com>
2023-04-08 05:44:41 +0200

    back to svg + gray

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,28 +2,11 @@ export const classNames = (...classes: string[]) => {
   return classes.filter(Boolean).join(' ');
 };
 
-const createSolidColorBase64 = (
+export const blurImage = (
   width: number,
   height: number,
-  color: string
-): string => {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-
-  canvas.width = width;
-  canvas.height = height;
-
-  ctx.fillStyle = color;
-  ctx.fillRect(0, 0, width, height);
-
-  return canvas.toDataURL('image/png');
-};
-
-export const blurImage = (w: number, h: number) => {
-  const defaultBase64Color =
-    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8DwHwAFBQIAZQ6iSAAAAABJRU5ErkJggg==';
-  if (typeof window !== 'undefined') {
-    return createSolidColorBase64(w, h, '#fff2');
-  }
-  return defaultBase64Color;
+  color: string = '#9994'
+) => {
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}"><filter id="b" color-interpolation-filters="sRGB"><feGaussianBlur stdDeviation=".5"></feGaussianBlur><feComponentTransfer><feFuncA type="discrete" tableValues="1 1"></feFuncA></feComponentTransfer></filter><rect width="${width}" height="${height}" fill="${color}" filter="url(#b)"></rect></svg>`;
+  return `data:image/svg+xml;base64,${Buffer.from(svg).toString('base64')}`;
 };
