048aa6610641cb12d197c7c3dd9abb9dd262e9ff
M4p4 <homejr@protonmail.com>
2023-04-08 05:49:45 +0200

    darker color for blur

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -5,7 +5,7 @@ export const classNames = (...classes: string[]) => {
 export const blurImage = (
   width: number,
   height: number,
-  color: string = '#9994'
+  color: string = '#444444'
 ) => {
   const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}"><filter id="b" color-interpolation-filters="sRGB"><feGaussianBlur stdDeviation=".5"></feGaussianBlur><feComponentTransfer><feFuncA type="discrete" tableValues="1 1"></feFuncA></feComponentTransfer></filter><rect width="${width}" height="${height}" fill="${color}" filter="url(#b)"></rect></svg>`;
   return `data:image/svg+xml;base64,${Buffer.from(svg).toString('base64')}`;
