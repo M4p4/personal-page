158edff070c65c85a544657fa573fea2e97e3cfa
M4p4 <homejr@protonmail.com>
2026-09-14 22:20:53 +0200

    code highlighting support + more markdown stuff

--- components/ui/MDXComponents.tsx
+++ components/ui/MDXComponents.tsx
@@ -1,4 +1,5 @@
-import { blurImage } from 'lib/helpers';
+import { CheckIcon } from '@heroicons/react/20/solid';
+import { blurImage, classNames } from 'lib/helpers';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { ComponentPropsWithoutRef } from 'react';
@@ -22,6 +23,27 @@ const MDXComponents = {
         </span>
       )}
     </span>
+  ),
+  input: (props: ComponentPropsWithoutRef<'input'>) =>
+    props.type === 'checkbox' ? (
+      <span
+        className={classNames(
+          'mt-[5px] inline-flex size-4.5 shrink-0 items-center justify-center rounded-md border',
+          props.checked
+            ? 'border-orange-500 bg-orange-500 text-white dark:border-orange-400 dark:bg-orange-400 dark:text-zinc-900'
+            : 'border-slate-400 bg-white dark:border-zinc-600 dark:bg-zinc-800',
+        )}
+      >
+        {props.checked && <CheckIcon aria-hidden className="size-3.5" />}
+        <span className="sr-only">{props.checked ? 'Done:' : 'Open:'}</span>
+      </span>
+    ) : (
+      <input {...props} />
+    ),
+  table: (props: ComponentPropsWithoutRef<'table'>) => (
+    <div className="overflow-x-auto">
+      <table {...props} />
+    </div>
   ),
   a: (props: ComponentPropsWithoutRef<'a'>) => (
     <Link href={props.href ?? ''} className="text-teal-700 dark:text-teal-500">
